e46182102863f486e29a0d75a542323ea6895fd0
heinz <heinzenlucas04@gmail.com>
2024-11-17 16:24:43 -0300

    Commit

---
+++ src/main/java/modelo/Jugador.java
@@ -0,0 +1,25 @@
+package modelo;
+
+public class Jugador {
+    private int idJugador;
+    private String nombreUsuario;
+    private String correo;
+    private String contraseña;
+
+    public Jugador(int idJugador, String nombreUsuario, String correo, String contraseña) {
+        this.idJugador = idJugador;
+        this.nombreUsuario = nombreUsuario;
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+
+    // Getters y Setters
+    public int getIdJugador() { return idJugador; }
+    public void setIdJugador(int idJugador) { this.idJugador = idJugador; }
+    public String getNombreUsuario() { return nombreUsuario; }
+    public void setNombreUsuario(String nombreUsuario) { this.nombreUsuario = nombreUsuario; }
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
+    public String getContraseña() { return contraseña; }
+    public void setContraseña(String contraseña) { this.contraseña = contraseña; }
+}
